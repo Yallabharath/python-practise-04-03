c36713eb8767451dc49c9b9d7a2f1c849aba71d6
Yallabharath <yallabharath64@gmail.com>
2025-03-06 22:42:47 +0530

    python ps

---
+++ ind.py
@@ -0,0 +1,54 @@
+# list = [1,2,3,4,5]
+# sum=0
+# for i in list:
+#  sum = sum+i
+# print(sum)
+
+
+# list1=[1,2,3,4,5]
+# min=list1[0]
+# max=list1[0]
+# for i in range(0,len(list1)):
+#     if  list1[i]<min:
+#          min =  list1[i]
+#     if  list1[i]>max:
+#         max =  list1[i]
+# print(min,max)
+
+
+# list2=[1,2,3,4,7]
+# min_num = list2[0]
+# max_num = list2[0]
+# for i in list2:
+#     if i<min:
+#         min_num = i
+#     if i>max:
+#         max_num = i
+# print(min_num,max_num)
+
+
+# list4=[2,2,32,-31,-7,29,28,7,7,32,-2,1]
+    
+# for i in range(len(list4)):
+#     flag = True
+#     for j in range(len(list4)):
+#         if list4[i] == list4[j]and i!=j:
+#               flag==False
+#               print(list4[i],'Is duplicate')
+#         break
+#     else:
+#         print(list4[j],'Is unique')
+
+
+# k = int(input("enter the index value to find the element in that index"))
+# list =[1,2,3,4,5,6]
+# if k>-1*len(list) and k<len(list):
+#     print(list[k])
+# else:
+#     print("index is out of range please enter with in the length of list range")
+
+# example-2
+
+# list = [4,5,6,7,9]
+# print(list[3])
+# print(list[-1])
